8175122906cef8d0957c2cef99173a882ecfdf0b
ruseto <rusetobalsamico1@gmail.com>
2024-05-27 12:06:09 -0300

    build

---
+++ build/precache-manifest.244e336d546c393cb21c0a6de9a78dce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "357a4b4edb7054a3ecd0",
+    "url": "/static/css/main.7ac2b9a8.chunk.css"
+  },
+  {
+    "revision": "357a4b4edb7054a3ecd0",
+    "url": "/static/js/main.357a4b4e.chunk.js"
+  },
+  {
+    "revision": "2a4eb8a055b70f360807",
+    "url": "/static/js/1.2a4eb8a0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "975263fb1e0505bf5262ba6fbe9260e4",
+    "url": "/index.html"
+  }
+];
